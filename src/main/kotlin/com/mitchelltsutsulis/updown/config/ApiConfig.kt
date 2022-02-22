8b1223647c89d040787766ed1ba6d6cdd3c6ac15
Mitchell Tsutsulis <mitchell444999@gmail.com>
2022-02-22 13:48:51 +1100

    Fixed broken config binding

--- src/main/kotlin/com/mitchelltsutsulis/updown/config/ApiConfig.kt
+++ src/main/kotlin/com/mitchelltsutsulis/updown/config/ApiConfig.kt
@@ -1,10 +1,8 @@
 package com.mitchelltsutsulis.updown.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@EnableConfigurationProperties
 @ConfigurationProperties(prefix="api")
-data class ApiConfig(val fileStoragePath: String = "/opt/updown/storage/")
+data class ApiConfig(var fileStoragePath: String = "/opt/updown/storage/")
